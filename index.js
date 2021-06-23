1fc26cff5649efcb723593967cea3eac46e6652f
ignaciocuadros <ignaciocuadros@gmail.com>
2021-06-23 14:56:36 +0200

    many ui changes

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-// window.addEventListener("load", (event) => {
-//   init();
-// }, false);
+let body = undefined;
+let main = undefined;
 
-// function init(){
-//   contactStylesWhenIntersectMain();
-// }
+window.addEventListener("load", () => {
+  main = document.querySelector("main");
+  window.addEventListener("scroll", () => {
+    toggleOverflowBodyAndMain();
+  });
+});
 
-// const contactElement = document.querySelector(".contact");
-// const mainElement = document.querySelector("main");
 
-// function contactStylesWhenIntersectMain(){
-//   if(contactElement.getBoundingClientRect().bottom >= mainElement.getBoundingClientRect().top){
-//     contactElement.classList.add('highlight');
-//   } else {
-//     recallFunction();
-//   }
-// }
+const toggleOverflowBodyAndMain = () => {
+  const isInViewport = this.isInViewport(main);
+  if(isInViewport){
+    main.classList.remove("overflowHidden");
+    main.classList.remove("marginScrollbar");
+  } else {
+    main.classList.add("overflowHidden");
+    setTimeout(() => {
+      toggleOverflowBodyAndMain();
+    }, 100);
+  }
+}
 
-// function recallFunction(){
-//   setTimeout(() => {
-//     contactStylesWhenIntersectMain();
-//   }, 250);
-// }
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      parseInt(rect.bottom) <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
